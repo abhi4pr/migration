1660f3cbe06548352f4fbd5636466cdaf7e45d10
saiminfinite <saimyual107@gmail.com>
2023-11-29 19:22:12 +0530

    worked on planOverView

--- src/components/AdminPanel/RegisterCampaign/PlanOverview.jsx
+++ src/components/AdminPanel/RegisterCampaign/PlanOverview.jsx
@@ -1,78 +1,92 @@
-
-import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import { DataGrid } from "@mui/x-data-grid";
-import { Link } from "react-router-dom";
-import SendTwoToneIcon from '@mui/icons-material/SendTwoTone';
-import Paper from "@mui/material/Paper";
-import TextField from "@mui/material/TextField";
-import { useState } from "react";
-function PlanOverview() {
-    const [searchText, setSearchText] = useState("");
-    const handleSearchChange = (event) => {
-      setSearchText(event.target.value);
-    };
+import { Box, Paper, TextField, Button, Tooltip } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+
+const PlanOverview = () => {
+  const [selectData, setSelectData] = useState([]);
+  const param = useParams();
+  const id = param.id;
+  console.log(selectData);
+  const getSelectPage = async () => {
+    const newPlan = await axios.get(
+      `http://34.93.135.33:8080/api/campaignplan/${id}`
+    );
+    setSelectData(newPlan.data.data);
+  };
+
+  useEffect(() => {
+    getSelectPage();
+  }, []);
+
   const columns = [
     {
-        field: "S.NO",
-        headerName: "S.NO",
-        width: 70,
-        editable: false,
-        renderCell: (params) => {
-          const rowIndex = rows.indexOf(params.row);
-          return <div>{rowIndex + 1}</div>;
-        },
+      field: "S.NO",
+      headerName: "S.NO",
+      width: 90,
+      editable: false,
+      renderCell: (params) => {
+        const rowIndex = selectData.indexOf(params.row);
+        return <div>{rowIndex + 1}</div>;
       },
-    { field: "firstName", headerName: "First name", width: 150 },
-    { field: "lastName", headerName: "Last name", width: 150 },
+    },
     {
-        field: "plan_overview",
-        headerName: "Plan Overview",
-        width: 150,
-        renderCell: () => {
-          return (
-            <div className="d-flex text-center align-item-center justify-content-center">
-              <Link to="/admin/phase">
-              <Button type="button">
-                <SendTwoToneIcon />
-              </Button>
-              </Link>
-            </div>
-          );
-        },
-       // width: 150,
-      },
+      field: "page_name",
+      headerName: "Page Name",
+      width: 150,
+    },
+
+    {
+      field: "cat_name",
+      headerName: "Category Name",
+      width: 150,
+    },
+    {
+      field: "follower_count",
+      headerName: "Follower Count",
+      width: 150,
+    },
+    {
+      field: "postPerPage",
+      headerName: "post / Page",
+      width: 150,
+    },
+
   ];
-  const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon" },
-    { id: 2, lastName: "Lannister", firstName: "Cersei"},
-    { id: 3, lastName: "Lannister", firstName: "Jaime" },
-  ];
-  const filteredRows = rows.filter((row) => {
-    return (
-      row.firstName.toLowerCase().includes(searchText.toLowerCase()) ||
-      row.lastName.toLowerCase().includes(searchText.toLowerCase())
-    );
-  });
+
   return (
-    <>
-          <Paper>
-        <div className="form-heading">
-          <div className="form_heading_title">
-            <h2> Plan Overview </h2>
-          </div>
-          </div>
-      </Paper>
-      <TextField
-        label="Search"
-        variant="outlined"
-        value={searchText}
-        onChange={handleSearchChange}
-        style={{ margin: "10px" }}
+    <Paper >
+      <div>
+        <div className="form_heading_title">
+          <h2 className="form-heading">Plan Overview</h2>
+        </div>
+      </div>
+      <Box sx={{ p: 2, m: 2 }}>
+        <label style={{ marginTop: "14px", marginRight: "10px", fontSize: "20px" }}>Plan Name : </label>
+        <TextField
+          disabled
+          id="outlined-disabled"
+          value={selectData[0]?.planName}
+        />
+       <Tooltip title="Delete Plan">
+  <Button type="button">
+    <DeleteIcon />
+  </Button>
+</Tooltip>
+
+      </Box>
+      <DataGrid
+        rows={selectData}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        getRowId={(row) => row.p_id}
       />
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGrid rows={filteredRows} columns={columns} pageSize={5} />
-    </div>
-    </>
+    </Paper>
   );
-}
+};
+
 export default PlanOverview;
