6671347ccafb079bd69f1fad055dbebe8c056908
abhi4pr <ascs739@gmail.com>
2023-12-01 17:36:36 +0530

    done ui part for 6 files for finance

--- src/components/AdminPanel/Finance/IncentivePayment.jsx
+++ src/components/AdminPanel/Finance/IncentivePayment.jsx
@@ -5,6 +5,7 @@ import jwtDecode from "jwt-decode";
 import FormContainer from "../FormContainer";
 import FieldContainer from "../FieldContainer";
 import { useGlobalContext } from "../../../Context/Context";
+import DataTable from "react-data-table-component";
 
 const IncentivePayment = () => {
   
@@ -13,7 +14,11 @@ const IncentivePayment = () => {
   const [heading, setHeading] = useState("");
   const [headingDesc, setHeadingDesc] = useState("");
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-
+  const [datas, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [contextData, setDatas] = useState([]);
+  const [filterData, setFilterData] = useState([]);
+  
   const token = sessionStorage.getItem("token");
   const decodedToken = jwtDecode(token);
   const loginUserId = decodedToken.id;
@@ -29,17 +34,107 @@ const IncentivePayment = () => {
       setIsFormSubmitted(true);
   };
 
-  if (isFormSubmitted) {
-    return <Navigate to="/admin/finance" />;
+  function getData() {
+    axios.get("http://34.93.135.33:8080/api/get_all_sims").then((res) => {
+      setData(res.data.data);
+      setFilterData(res.data.data);
+    });
   }
 
-  const handleDateChange = (e) => {
-    
-  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const result = datas.filter((d) => {
+      return (
+        d.assetsName?.toLowerCase().match(search.toLowerCase())
+      );
+    });
+    setFilterData(result);
+  }, [search]);
+
+  const columns = [
+    {
+      name: "S.No",
+      cell: (row, index) => <div>{index + 1}</div>,
+      width: "9%",
+      sortable: true,
+    },
+    {
+      name: "Customer Name",
+      selector: (row) => row.assetsName,
+      sortable: true,
+    },
+    {
+      name: "Sales executive name",
+      selector: (row) => row.sub_category_name,
+    },
+    {
+      name: "Sale booking date",
+      selector: (row) => row.category_name,
+    },
+    {
+      name: "campaign amount",
+      selector: (row) => row.vendor_name,
+    },
+    {
+      name: "paid amount",
+      selector: (row) =>row.dateOfPurchase 
+    },
+    {
+      name: "balance amount",
+      selector: (row) => row.paymentAmount
+    },
+    {
+      name: "Action",
+      selector: (row) => (
+          <button
+            className="btn btn-outline-success"
+            // onClick={() => handleImageClick(row)}
+          >
+            Balance Update
+          </button>
+        ),
+    }
+  ];
 
   return (
     <>
-      <h3>IncentivePayment</h3>
+      <FormContainer
+        mainTitle="Incentive payment list"
+        link="/admin/incentive-payment-list"
+        buttonAccess={
+          contextData &&
+          contextData[2] &&
+          contextData[2].insert_value === 1 &&
+          false
+        }
+      />
+
+      <div className="card">
+        <div className="data_tbl table-responsive">
+          <DataTable
+            title="Incentive payment list"
+            columns={columns}
+            data={filterData}
+            fixedHeader
+            // pagination
+            fixedHeaderScrollHeight="64vh"
+            highlightOnHover
+            subHeader
+            subHeaderComponent={
+              <input
+                type="text"
+                placeholder="Search here"
+                className="w-50 form-control"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            }
+          />
+        </div>
+      </div>
     </>
   );
 };
--- src/components/AdminPanel/Finance/PendingApprovalRefund.jsx
+++ src/components/AdminPanel/Finance/PendingApprovalRefund.jsx
@@ -5,6 +5,7 @@ import jwtDecode from "jwt-decode";
 import FormContainer from "../FormContainer";
 import FieldContainer from "../FieldContainer";
 import { useGlobalContext } from "../../../Context/Context";
+import DataTable from "react-data-table-component";
 
 const PendingApprovalRefund = () => {
   
@@ -13,6 +14,10 @@ const PendingApprovalRefund = () => {
   const [heading, setHeading] = useState("");
   const [headingDesc, setHeadingDesc] = useState("");
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [datas, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [contextData, setDatas] = useState([]);
+  const [filterData, setFilterData] = useState([]);
 
   const token = sessionStorage.getItem("token");
   const decodedToken = jwtDecode(token);
@@ -29,17 +34,107 @@ const PendingApprovalRefund = () => {
       setIsFormSubmitted(true);
   };
 
-  if (isFormSubmitted) {
-    return <Navigate to="/admin/finance" />;
+  function getData() {
+    axios.get("http://34.93.135.33:8080/api/get_all_sims").then((res) => {
+      setData(res.data.data);
+      setFilterData(res.data.data);
+    });
   }
 
-  const handleDateChange = (e) => {
-    
-  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const result = datas.filter((d) => {
+      return (
+        d.assetsName?.toLowerCase().match(search.toLowerCase())
+      );
+    });
+    setFilterData(result);
+  }, [search]);
+
+  const columns = [
+    {
+      name: "S.No",
+      cell: (row, index) => <div>{index + 1}</div>,
+      width: "9%",
+      sortable: true,
+    },
+    {
+      name: "Customer Name",
+      selector: (row) => row.assetsName,
+      sortable: true,
+    },
+    {
+      name: "Refund Amount",
+      selector: (row) => row.sub_category_name,
+    },
+    {
+      name: "Finance refund reason",
+      selector: (row) => row.category_name,
+    },
+    {
+      name: "refund request date",
+      selector: (row) => row.vendor_name,
+    },
+    {
+      name: "refund updated date",
+      selector: (row) =>row.dateOfPurchase 
+    },
+    {
+      name: "refund Payment image",
+      selector: (row) => row.paymentAmount
+    },
+    {
+      name: "Action",
+      selector: (row) => (
+          <button
+            className="btn btn-outline-success"
+            // onClick={() => handleImageClick(row)}
+          >
+            Summary
+          </button>
+        ),
+    }
+  ];
 
   return (
     <>
-      <h3>PendingApprovalRefund</h3>
+      <FormContainer
+        mainTitle="Pending Approval for refund"
+        link="/admin/finance-pedingapprovalrefund"
+        buttonAccess={
+          contextData &&
+          contextData[2] &&
+          contextData[2].insert_value === 1 &&
+          false
+        }
+      />
+
+      <div className="card">
+        <div className="data_tbl table-responsive">
+          <DataTable
+            title="Pending Approval for refund"
+            columns={columns}
+            data={filterData}
+            fixedHeader
+            // pagination
+            fixedHeaderScrollHeight="64vh"
+            highlightOnHover
+            subHeader
+            subHeaderComponent={
+              <input
+                type="text"
+                placeholder="Search here"
+                className="w-50 form-control"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            }
+          />
+        </div>
+      </div>
     </>
   );
 };
